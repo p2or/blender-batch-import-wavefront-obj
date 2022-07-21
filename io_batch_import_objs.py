d6e71d41a13e2b6fb014c6498a94125629bcc037
p2or <512368+p2or@users.noreply.github.com>
2022-07-21 23:04:25 +0200

    Blender 2.80 updates

--- io_batch_import_objs.py
+++ io_batch_import_objs.py
@@ -20,8 +20,8 @@
 bl_info = {
     "name": "Batch Import Wavefront (.obj)",
     "author": "p2or",
-    "version": (0, 1, 0),
-    "blender": (2, 65, 0),
+    "version": (0, 2, 0),
+    "blender": (2, 80, 0),
     "location": "File > Import-Export",
     "description": "Import multiple OBJ files, their UV's and materials",
     "warning": "",
@@ -44,7 +44,7 @@ from bpy.props import (BoolProperty,
 
 
 class ImportMultipleObjs(bpy.types.Operator, ImportHelper):
-    """This appears in the tooltip of the operator and in the generated docs"""
+    """Batch Import Wavefront obj"""
     bl_idname = "import_scene.multiple_objs"
     bl_label = "Import multiple OBJ's"
     bl_options = {'PRESET', 'UNDO'}
@@ -58,65 +58,55 @@ class ImportMultipleObjs(bpy.types.Operator, ImportHelper):
             )
 
     # Selected files
-    files = CollectionProperty(type=bpy.types.PropertyGroup)
+    files: CollectionProperty(type=bpy.types.PropertyGroup)
 
     # List of operator properties, the attributes will be assigned
     # to the class instance from the operator settings before calling.
-    ngons_setting = BoolProperty(
-            name="NGons",
-            description="Import faces with more than 4 verts as ngons",
-            default=True,
-            )
-    edges_setting = BoolProperty(
+    edges_setting: BoolProperty(
             name="Lines",
             description="Import lines and faces with 2 verts as edge",
             default=True,
             )
-    smooth_groups_setting = BoolProperty(
+    smooth_groups_setting: BoolProperty(
             name="Smooth Groups",
             description="Surround smooth groups by sharp edges",
             default=True,
             )
-
-    split_objects_setting = BoolProperty(
+    split_objects_setting: BoolProperty(
             name="Object",
             description="Import OBJ Objects into Blender Objects",
             default=True,
             )
-    split_groups_setting = BoolProperty(
+    split_groups_setting: BoolProperty(
             name="Group",
             description="Import OBJ Groups into Blender Objects",
             default=True,
             )
-
-    groups_as_vgroups_setting = BoolProperty(
+    groups_as_vgroups_setting: BoolProperty(
             name="Poly Groups",
             description="Import OBJ groups as vertex groups",
             default=False,
             )
-
-    image_search_setting = BoolProperty(
+    image_search_setting: BoolProperty(
             name="Image Search",
             description="Search subdirs for any associated images "
                         "(Warning, may be slow)",
             default=True,
             )
-
-    split_mode_setting = EnumProperty(
+    split_mode_setting: EnumProperty(
             name="Split",
             items=(('ON', "Split", "Split geometry, omits unused verts"),
                    ('OFF', "Keep Vert Order", "Keep vertex order from file"),
                    ),
             )
-
-    clamp_size_setting = FloatProperty(
+    clamp_size_setting: FloatProperty(
             name="Clamp Size",
             description="Clamp bounds under this value (zero to disable)",
             min=0.0, max=1000.0,
             soft_min=0.0, soft_max=1000.0,
             default=0.0,
             )
-    axis_forward_setting = EnumProperty(
+    axis_forward_setting: EnumProperty(
             name="Forward",
             items=(('X', "X Forward", ""),
                    ('Y', "Y Forward", ""),
@@ -127,8 +117,7 @@ class ImportMultipleObjs(bpy.types.Operator, ImportHelper):
                    ),
             default='-Z',
             )
-
-    axis_up_setting = EnumProperty(
+    axis_up_setting: EnumProperty(
             name="Up",
             items=(('X', "X Up", ""),
                    ('Y', "Y Up", ""),
@@ -144,10 +133,8 @@ class ImportMultipleObjs(bpy.types.Operator, ImportHelper):
         layout = self.layout
 
         row = layout.row(align=True)
-        row.prop(self, "ngons_setting")
+        row.prop(self, "smooth_groups_setting")
         row.prop(self, "edges_setting")
-
-        layout.prop(self, "smooth_groups_setting")
 
         box = layout.box()
         row = box.row()
@@ -161,26 +148,28 @@ class ImportMultipleObjs(bpy.types.Operator, ImportHelper):
         else:
             row.prop(self, "groups_as_vgroups_setting")
 
-        row = layout.split(percentage=0.67)
+        row = layout.split(factor=0.67)
         row.prop(self, "clamp_size_setting")
         layout.prop(self, "axis_forward_setting")
         layout.prop(self, "axis_up_setting")
 
         layout.prop(self, "image_search_setting")
 
+
     def execute(self, context):
 
         # get the folder
-        folder = (os.path.dirname(self.filepath))
+        folder = os.path.dirname(self.filepath)
 
         # iterate through the selected files
         for i in self.files:
 
             # generate full path to file
-            path_to_file = (os.path.join(folder, i.name))
+            path_to_file = os.path.join(folder, i.name)
 
             # call obj operator and assign ui values                  
-            bpy.ops.import_scene.obj(filepath = path_to_file,
+            bpy.ops.import_scene.obj(
+                                filepath = path_to_file,
                                 axis_forward = self.axis_forward_setting,
                                 axis_up = self.axis_up_setting, 
                                 use_edges = self.edges_setting,
@@ -190,24 +179,23 @@ class ImportMultipleObjs(bpy.types.Operator, ImportHelper):
                                 use_groups_as_vgroups = self.groups_as_vgroups_setting,
                                 use_image_search = self.image_search_setting,
                                 split_mode = self.split_mode_setting,
-                                global_clamp_size = self.clamp_size_setting)
+                                global_clight_size = self.clamp_size_setting)
 
         return {'FINISHED'}
 
 
 # Only needed if you want to add into a dynamic menu
 def menu_func_import(self, context):
-    self.layout.operator(ImportMultipleObjs.bl_idname, text="Wavefont Batch (.obj)")
+    self.layout.operator(ImportMultipleObjs.bl_idname, text="Wavefront Batch (.obj)")
 
 
 def register():
     bpy.utils.register_class(ImportMultipleObjs)
-    bpy.types.INFO_MT_file_import.append(menu_func_import)
-
+    bpy.types.TOPBAR_MT_file_import.append(menu_func_import)
 
 def unregister():
     bpy.utils.unregister_class(ImportMultipleObjs)
-    bpy.types.INFO_MT_file_import.remove(menu_func_import)
+    bpy.types.TOPBAR_MT_file_import.remove(menu_func_import)
 
 
 if __name__ == "__main__":
